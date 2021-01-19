bbb61b06b9a9c878f962c0f85479a3ec726a6bef
Harel Lustiger <tidylab@gmail.com>
2021-01-19 09:20:02 +0000

    version 0.1.1

---
+++ README.md
@@ -0,0 +1,53 @@
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+`R6P` <img src="https://raw.githubusercontent.com/tidylab/R6P/master/pkgdown/logo.png" align="right" height="75"/>
+==================================================================================================================
+
+<!-- badges: start -->
+
+[![CRAN
+status](https://www.r-pkg.org/badges/version/R6P)](https://CRAN.R-project.org/package=R6P)
+[![R build
+status](https://github.com/tidylab/R6P/workflows/R-CMD-check/badge.svg)](https://github.com/tidylab/R6P/actions)
+[![codecov](https://codecov.io/gh/tidylab/R6P/branch/master/graph/badge.svg?token=U6FL5N32FL)](https://codecov.io/gh/tidylab/R6P)
+
+<!-- badges: end -->
+
+Introduction
+------------
+
+Build robust and maintainable software with object-oriented design
+patterns in R. Design patterns abstract and present in neat,
+well-defined components and interfaces the experience of many software
+designers and architects over many years of solving similar problems.
+These are solutions that have withstood the test of time with respect to
+re-usability, flexibility, and maintainability. ‘R6P’ provides abstract
+base classes with examples for a few known design patterns. The patterns
+were selected by their applicability to analytic projects in R. Using
+these patterns in R projects have proven effective in dealing with the
+complexity that data-driven applications possess.
+
+This package is based on the work of Gamma1995, and Fowler2002.
+
+Installation
+------------
+
+You can install the released version of R6P from
+[CRAN](https://CRAN.R-project.org) with:
+
+    install.packages("R6P")
+
+And the development version from [GitHub](https://github.com/) with:
+
+    # install.packages("devtools")
+    devtools::install_github("tidylab/R6P")
+
+References
+----------
+
+Fowler, Martin. 2002. *Patterns of enterprise application architecture*.
+Addison-Wesley Longman Publishing Co., Inc.
+
+Gamma, Erich, Richard Helm, Ralph Johnson, and John Vlissides. 1995.
+*Design patterns: elements of reusable object-oriented software*.
+Pearson Education India.
---
+++ tests/testthat/test-base-Singleton.R
@@ -0,0 +1,55 @@
+# General -----------------------------------------------------------------
+test_that("calling Singleton$new fails because it cannot be instantiated directly", {
+    expect_error(Singleton$new())
+})
+
+# Implementation ----------------------------------------------------------
+test_that("instantiating of multiple objects of the same Singleton are identical", {
+    Counter <- R6::R6Class(classname = "Counter", inherit = Singleton, public = list(
+        count = 0,
+        add_1 = function(){self$count = self$count + 1; invisible(self)}
+    ))
+
+    expect_s3_class(counter_1 <- Counter$new(), "Singleton")
+    expect_s3_class(counter_2 <- Counter$new(), "Counter")
+    expect_identical(counter_1, counter_2)
+
+    counter_1$add_1()
+    expect_equal(counter_1$count, counter_2$count)
+})
+
+test_that("instantiating of multiple objects of the same Singleton with superclass", {
+    SuperCounter <- R6::R6Class(classname = "SuperCounter", inherit = Singleton, public = list(
+        count = 0,
+        add_1 = function(){self$count = self$count + 1; invisible(self)},
+        initialize = function(){super$initialize()}
+    ))
+
+    expect_s3_class(counter_1 <- SuperCounter$new(), "Singleton")
+    expect_s3_class(counter_2 <- SuperCounter$new(), "SuperCounter")
+    expect_identical(counter_1, counter_2)
+
+    counter_1$add_1()
+    expect_equal(counter_1$count, counter_2$count)
+})
+
+test_that("instantiating of multiple objects of the different Singleton are not identical", {
+    SingletonA <- R6::R6Class(classname = "SingletonA", inherit = Singleton, public = list(uid = "A"))
+    SingletonB <- R6::R6Class(classname = "SingletonB", inherit = Singleton, public = list(uid = "B"))
+
+    expect_s3_class(singleton_A <- SingletonA$new(), "Singleton")
+    expect_s3_class(singleton_B <- SingletonB$new(), "Singleton")
+    expect_false(identical(singleton_A, singleton_B))
+})
+
+# test_that("inheriting Singleton takes the last class name", {
+#     Level1Class <- R6::R6Class(classname = "Level1", inherit = Singleton, public = list(uid = "A"))
+#     expect_s3_class(level1 <- Level1Class$new(), "Level1")
+#
+#     Level2Class <- R6::R6Class(classname = "Level2", inherit = Level1Class, public = list(uid = "A"))
+#     expect_s3_class(level2 <- Level2Class$new(), "Level2")
+#
+#     expect_false(identical(level1, level2))
+# })
+
+
